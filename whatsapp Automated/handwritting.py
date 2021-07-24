17f522ea36a7926b92f1be3f4f1a65a06e3a8af4
SAURAV SAGAR <64888744+sauravsagar19@users.noreply.github.com>
2021-07-24 22:46:20 +0530

    Add files via upload

---
+++ whatsapp Automated/handwritting.py
@@ -0,0 +1,11 @@
+## for playing youtube vedios from the python code
+import pywhatkit as kit
+# pywhatkit.playonyt("https://www.youtube.com/badsaah")
+
+# print("Successfully played!")
+a='''
+hello, my name is saurav sagar...... :)
+'''
+kit.text_to_handwriting(a)
+
+print("Successfully done !")
