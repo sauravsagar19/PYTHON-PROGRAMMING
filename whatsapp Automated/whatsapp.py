02a118a54a9784240322fb13b30f0c57b6a1d680
SAURAV SAGAR <64888744+sauravsagar19@users.noreply.github.com>
2021-07-24 16:53:48 +0530

    Add files via upload

---
+++ whatsapp Automated/whatsapp.py
@@ -0,0 +1,8 @@
+
+import pywhatkit
+
+
+# pywhatkit.sendwhatmsg("+918210386150"," Welcome to the world of Automation... Currently you are Expriencing the Whatsapp Automation \n Welcome to the world of Automation... Currently you are Expriencing the Whatsapp Automation \n Welcome to the world of Automation... Currently you are Expriencing the Whatsapp Automation \n Welcome to the world of Automation... Currently you are Expriencing the Whatsapp Automation \n Welcome to the world of Automation... Currently you are Expriencing the Whatsapp Automation \n Welcome to the world of Automation... Currently you are Expriencing the Whatsapp Automation \n Welcome to the world of Automation... Currently you are Expriencing the Whatsapp Automation",13, 54)
+pywhatkit.playonyt("https://www.youtube.com/badsaah")
+
+print("Successfully played!")
